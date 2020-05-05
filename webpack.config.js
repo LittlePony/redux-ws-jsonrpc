a38ab913ba1780002dc5fba1c0f58470c8ccebde
Sergey D <a1ia5@mail.ru>
2020-05-06 02:42:27 +0300

    fix quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const { NODE_ENV: env = 'development' } = process.env;
+const { NODE_ENV: env = "development" } = process.env;
 
 module.exports = {
     mode: env,
-    entry: './src/index',
+    entry: "./src/index",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2',
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+        libraryTarget: "commonjs2",
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
+        extensions: [".ts", ".tsx", ".js", ".json"],
     },
     module: {
         rules: [
             {
                 test: /\.(ts|js)x?$/,
-                include: path.resolve(__dirname, 'src'),
-                loader: 'babel-loader',
+                include: path.resolve(__dirname, "src"),
+                loader: "babel-loader",
             },
         ],
     },
